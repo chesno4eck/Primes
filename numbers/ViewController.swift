d577ec69349348048e62e44b6eb4fed4c16be4c4
du.aliev <du.aliev@tensor.ru>
2017-09-15 11:20:54 +0700

    рефакторинг проекта, ui части. доработан поиск

--- numbers/ViewController.swift
+++ numbers/ViewController.swift
@@ -21,89 +21,99 @@ class ViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		setupUI()
+		setupObserversAndRecognizers()
+	}
+	
+	private func setupUI() {
 		title = "Поиск простых чисел"
 		searchBar.isHidden = true
-		
+		input.becomeFirstResponder()
+
 		input.keyboardType = .numberPad
 		searchBar.keyboardType = .numberPad
-		
+	}
+	
+	private func setupObserversAndRecognizers() {
 		let collectionViewTap = UITapGestureRecognizer(target: self, action: #selector(self.hideKeyboard))
 		collectionView.addGestureRecognizer(collectionViewTap)
 		
 		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: .UIKeyboardWillShow, object: nil)
 		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: .UIKeyboardWillHide, object: nil)
-
 	}
 	
-	
-	func keyboardWillShow(notification: NSNotification) {
-		if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-			collectionViewBottomConstraint.constant = keyboardSize.height
-		}
-	}
-	
-	func keyboardWillHide(_ : NSNotification) {
-		collectionViewBottomConstraint.constant = 8
-	}
-	
-	@objc fileprivate func hideKeyboard() {
-		view.endEditing(true)
-	}
 	
 	@IBAction fileprivate func calculateAndPrint(_ sender: Any) {
-		hideKeyboard()
-		activityIndicator.startAnimating()
-		startButton.isEnabled = false
+		startActivity()
 		
-		defer {
-			DispatchQueue.main.async {
-				self.activityIndicator.stopAnimating()
-				self.startButton.isEnabled = true
-			}
-		}
 		DispatchQueue.global(qos: .userInteractive).async {
 			guard self.input.text != nil && !self.input.text!.isEmpty else {
 				DispatchQueue.main.async {
 					ADAlerts.showOkAlert(on: self, title: "Нет числа", text: "Пожалуйста, введите целое положительное число")
 				}
+				self.endActivity()
 				return
 			}
 			guard let number = Int(self.input.text!) else {
 				DispatchQueue.main.async {
 					ADAlerts.showOkAlert(on: self, title: "Так не пойдет", text: "\"\(self.input.text!)\" - это не число")
 				}
+				self.endActivity()
 				return
 			}
 			guard number > 2 else {
 				DispatchQueue.main.async {
 					ADAlerts.showOkAlert(on: self, title: "Число слишком маленькое", text: "Пожалуйста, введите число больше чем 2")
 				}
+				self.endActivity()
 				return
 			}
 			guard number < 20000001 else {
 				DispatchQueue.main.async {
 					ADAlerts.showOkAlert(on: self, title: "Число слишком большое", text: "Пожалуйста, введите число меньше чем 20.000.000")
 				}
+				self.endActivity()
 				return
 			}
 			
 			self.primes = PrimesGenerator.primes(n: number)
 			
 			DispatchQueue.main.async {
+				self.endActivity()
 				self.searchBar.isHidden = false
 				self.collectionView.reloadData()
 			}
 		}
 	}
-}
 
-extension ViewController: UITextFieldDelegate {
+	private func startActivity() {
+		hideKeyboard()
+		activityIndicator.startAnimating()
+		startButton.isEnabled = false
+	}
+
+	private func endActivity() {
+		self.startButton.isEnabled = true
+		self.activityIndicator.stopAnimating()
+	}
 	
-	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-		calculateAndPrint(self)
-		return true
+	private let bottomConstraintConstant: CGFloat = 8
+	
+	func keyboardWillShow(notification: NSNotification) {
+		if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+			collectionViewBottomConstraint.constant = keyboardSize.height + bottomConstraintConstant
+		}
+	}
+	
+	func keyboardWillHide(_ : NSNotification) {
+		collectionViewBottomConstraint.constant = bottomConstraintConstant
+	}
+	
+	@objc fileprivate func hideKeyboard() {
+		view.endEditing(true)
 	}
 }
+
 
 extension ViewController: UICollectionViewDataSource {
 	
@@ -113,13 +123,22 @@ extension ViewController: UICollectionViewDataSource {
 	
 	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
-		cell.label.text = String(primes[indexPath.row])
-		cell.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-		
+		if primes.count > indexPath.row {
+			cell.label.text = String(primes[indexPath.row])
+		}
 		cell.selectedBackgroundView = UIView(frame: cell.contentView.bounds)
 		cell.selectedBackgroundView!.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
 		
 		return cell
+	}
+}
+
+
+extension ViewController: UITextFieldDelegate {
+	
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		calculateAndPrint(self)
+		return true
 	}
 }
 
@@ -131,14 +150,17 @@ extension ViewController: UISearchBarDelegate {
 		guard let number = Int(searchText) else {
 			return
 		}
+		guard number > 1 else {
+			return
+		}
 		DispatchQueue.global(qos: .userInteractive).async {
 			let indexPath = ArrayFinder.index(of: number, in: self.primes)
 			DispatchQueue.main.async {
 				self.collectionView.allowsMultipleSelection = false
-				self.collectionView.selectItem(at: indexPath, animated: true, scrollPosition: .centeredVertically)
+				self.collectionView.selectItem(at: indexPath, animated: true, scrollPosition: .top)
 				if number != self.primes[indexPath.row] && indexPath.row < self.collectionView.numberOfItems(inSection: 0) - 1 {
 					self.collectionView.allowsMultipleSelection = true
-					self.collectionView.selectItem(at: IndexPath(row: indexPath.row + 1, section: 0), animated: true, scrollPosition: .centeredVertically)
+					self.collectionView.selectItem(at: IndexPath(row: indexPath.row + 1, section: 0), animated: true, scrollPosition: UICollectionViewScrollPosition.init(rawValue: 0))
 				}
 			}
 		}
@@ -148,5 +170,3 @@ extension ViewController: UISearchBarDelegate {
 		hideKeyboard()
 	}
 }
-
-
